221b0d37c9be0a96d2600c7e3a36883e024eb129
Antoine Sesini <antoine.sesini@olvid.io>
2026-02-22 13:34:33 +0100

    Added bot logic and remove docker usage for bots. Docker is only used for the daemon gRPC server and sometimes for admin task with the cli.

--- tchouchou/main.py
+++ tchouchou/main.py
@@ -1,9 +1,14 @@
 import asyncio
-from olvid import OlvidClient
+from olvid import OlvidClient, datatypes, tools
+
+class Bot(OlvidClient):
+    async def on_message_received(self, message: datatypes.Message):
+        print(f"Message received: {message.body}")
 
 async def main():
-    client = OlvidClient()
-    print((await client.identity_get()).display_name)
+    bot = Bot()
+    tools.AutoInvitationBot()
+    await bot.run_forever()
 
 asyncio.set_event_loop(asyncio.new_event_loop())
 asyncio.get_event_loop().run_until_complete(main())
